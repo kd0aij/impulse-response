361dc5c284452224e3cd94c3085bab86aa740401
Mark Whitehorn <kd0aij@gmail.com>
2017-10-15 11:17:41 -0600

    clean up waterfall plot

--- src/RFexplorer/plot_all.m
+++ src/RFexplorer/plot_all.m
@@ -1,6 +1,28 @@
-basePath = "/home/markw/Desktop/RFexplorer/maxhold2"
+basePath = "/home/markw/Desktop/RFexplorer/test1"
+flist=dir(basePath);
 
-for i=[1:13]
-  plotRF(basePath, i)
-  input(':')
+nfreqs=0;
+nscans=0;
+fulldata=[];
+for i=[1:size(flist)]
+  if (findstr(flist(i).name, '.csv') > 10)
+    disp(flist(i).name);
+    [startdate, starttime, enddate, endtime, data]=plotRF2(basePath, flist(i).name);
+    if (nfreqs == 0)
+      nfreqs=size(data)(2)
+      startd=startdate
+      startt=starttime
+    endif
+    nscans+=size(data)(1)
+    fulldata = [fulldata; data];
+    fflush(stdout());
+  endif
 endfor
+endd=enddate
+endt=endtime
+figure()
+waterfall(fulldata)
+title("waterfall")
+xlabel("freq bin")
+ylabel("scan")
+zlabel("dBm")
